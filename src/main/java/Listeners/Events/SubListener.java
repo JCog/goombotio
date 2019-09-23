065130098b6239ea0433a7ffcc4446c13c7d1db5
JCog <jordancogan4@gmail.com>
2019-09-23 03:34:09 -0400

    sub gifting alert, including multiple subs

--- src/main/java/Listeners/Events/SubListener.java
+++ src/main/java/Listeners/Events/SubListener.java
@@ -6,12 +6,19 @@ import com.gikk.twirk.types.usernotice.Usernotice;
 import com.gikk.twirk.types.usernotice.subtype.Subscription;
 import com.gikk.twirk.types.users.TwitchUser;
 
+import java.util.*;
+
 import static java.lang.System.out;
 
 public class SubListener implements TwirkListener {
     private Twirk twirk;
+    private HashMap<String, Boolean> subTimersActive;
+    private HashMap<String, ArrayList<String>> giftedSubs;
+    
     public SubListener(Twirk twirk){
         this.twirk = twirk;
+        subTimersActive = new HashMap<>();
+        giftedSubs = new HashMap<>();
     }
     
     @Override
@@ -19,8 +26,31 @@ public class SubListener implements TwirkListener {
         if (usernotice.isSubscription()) {
             Subscription sub = usernotice.getSubscription().get();
             if (sub.isGift()) {
+                String gifterName = user.getDisplayName();
                 String recipientName = sub.getSubscriptionGift().get().getRecipiantDisplayName();
-                out.println(String.format("%s has gifted a sub to %s", user.getDisplayName(), recipientName));
+                out.println(String.format("%s has gifted a sub to %s", gifterName, recipientName));
+    
+                giftedSubs.computeIfAbsent(gifterName, k -> new ArrayList<>());
+                giftedSubs.get(gifterName).add(recipientName);
+                
+                if (subTimersActive.get(gifterName) == null || !subTimersActive.get(gifterName)) {
+                    subTimersActive.put(gifterName, true);
+                    new Timer().schedule(new TimerTask() {
+                        
+                        @Override
+                        public void run() {
+                            if (giftedSubs.get(gifterName).size() == 1) {
+                                twirk.channelMessage(String.format("jcogChamp @%s Thank you for gifting a sub to @%s! jcogChamp", gifterName, recipientName));
+                            }
+                            else {
+                                twirk.channelMessage(String.format("jcogChamp @%s Thank you for the %d gift subs! jcogChamp", gifterName, giftedSubs.get(gifterName).size()));
+                            }
+                            
+                            giftedSubs.remove(gifterName);
+                            subTimersActive.put(gifterName, false);
+                        }
+                    }, 500);
+                }
             }
             else {
                 int months = sub.getMonths();
