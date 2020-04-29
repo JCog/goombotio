1102317eb3c4823925e5882582cddc620fc0a3ee
JCog <jordancogan4@gmail.com>
2020-04-29 19:28:35 -0400

    begin TwitchUserLevel

---
+++ src/main/java/Util/TwitchUserLevel.java
@@ -0,0 +1,66 @@
+package Util;
+
+import com.gikk.twirk.types.users.TwitchUser;
+
+public class TwitchUserLevel {
+    public enum USER_LEVEL {
+        BROADCASTER(9),
+        MOD(6),
+        VIP(5),
+        STAFF(4),
+        SUBSCRIBER(2),
+        DEFAULT(0);
+    
+        public final int value;
+    
+        private USER_LEVEL(int value) {
+            this.value = value;
+        }
+    }
+    
+    public static USER_LEVEL getUserLevel(TwitchUser twitchUser) {
+        String[] badges = twitchUser.getBadges();
+        boolean broadcaster = false;
+        boolean mod = false;
+        boolean vip = false;
+        boolean staff = false;
+        boolean sub = false;
+        for (String badge : badges) {
+            switch (badge) {
+                case ("broadcaster"):
+                    broadcaster = true;
+                    break;
+                case ("moderator"):
+                    mod = true;
+                    break;
+                case ("vip"):
+                    vip = true;
+                    break;
+                case ("staff"):
+                    staff = true;
+                    break;
+                case ("subscriber"):
+                    sub = true;
+                    break;
+            }
+        }
+        if (broadcaster) {
+            return USER_LEVEL.BROADCASTER;
+        }
+        else if (mod) {
+            return USER_LEVEL.MOD;
+        }
+        else if (vip) {
+            return USER_LEVEL.VIP;
+        }
+        else if (staff) {
+            return USER_LEVEL.STAFF;
+        }
+        else if (sub) {
+            return USER_LEVEL.SUBSCRIBER;
+        }
+        else {
+            return USER_LEVEL.DEFAULT;
+        }
+    }
+}
