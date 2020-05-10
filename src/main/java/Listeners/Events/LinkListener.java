0c47876cfc0660212519b43e841b926d0f34f4a7
JCog <jordancogan4@gmail.com>
2020-05-10 19:08:20 -0400

    add twitter link listening

--- src/main/java/Listeners/Events/LinkListener.java
+++ src/main/java/Listeners/Events/LinkListener.java
@@ -8,6 +8,11 @@ import com.gikk.twirk.types.twitchMessage.TwitchMessage;
 import com.gikk.twirk.types.users.TwitchUser;
 import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.helix.domain.*;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 import java.text.NumberFormat;
 import java.util.ArrayList;
@@ -17,11 +22,13 @@ import java.util.regex.Pattern;
 
 public class LinkListener implements TwirkListener {
     private static final Pattern clipPattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.|clips\\.)?twitch\\.tv/(?:[a-zA-Z0-9_]+/clip/)?([a-zA-Z]+)\\S*");
-    private static final Pattern videoPattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.)?twitch\\.tv/videos/([0-9]+)");
-    private static final Pattern youtubePattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.)?(?:youtube\\.com/watch\\?v=|youtu\\.be/)([a-zA-Z0-9_\\-]+)");
+    private static final Pattern videoPattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.)?twitch\\.tv/videos/([0-9]+)\\S");
+    private static final Pattern youtubePattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.)?(?:youtube\\.com/watch\\?v=|youtu\\.be/)([a-zA-Z0-9_\\-]+)\\S*");
+    private static final Pattern tweetPattern = Pattern.compile("(?:https?://|^|\\s)+(?:www\\.)?(?:twitter\\.com/[a-zA-Z0-9_]+/status/)([0-9]+)\\S");
     
     private final TwirkInterface twirk;
     private final TwitchClient twitchClient;
+    private final Twitter twitter;
     private final String authToken;
     private final String youtubeApiKey;
     
@@ -30,13 +37,22 @@ public class LinkListener implements TwirkListener {
         this.twitchClient = twitchClient;
         this.authToken = Settings.getTwitchAuthToken();
         this.youtubeApiKey = Settings.getYoutubeApiKey();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(Settings.getTwitterConsumerKey())
+                .setOAuthConsumerSecret(Settings.getTwitterConsumerSecret())
+                .setOAuthAccessToken(Settings.getTwitterAccessToken())
+                .setOAuthAccessTokenSecret(Settings.getTwitterAccessTokenSecret());
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        twitter = tf.getInstance();
     }
     
     @Override
     public void onPrivMsg(TwitchUser sender, TwitchMessage message) {
-        ArrayList<String> clipUrls = getUrls(message.getContent(), clipPattern);
-        ArrayList<String> videoUrls = getUrls(message.getContent(), videoPattern);
-        ArrayList<String> youtubeVideoIds = getUrls(message.getContent(), youtubePattern);
+        ArrayList<String> clipUrls = getMatches(message.getContent(), clipPattern);
+        ArrayList<String> videoUrls = getMatches(message.getContent(), videoPattern);
+        ArrayList<String> youtubeVideoIds = getMatches(message.getContent(), youtubePattern);
+        ArrayList<String> tweetIds = getMatches(message.getContent(), tweetPattern);
         
         for (String id : clipUrls) {
             twirk.channelMessage(getClipDetails(id));
@@ -47,9 +63,12 @@ public class LinkListener implements TwirkListener {
         for (String id : youtubeVideoIds) {
             twirk.channelMessage(YoutubeApi.getVideoDetails(id, youtubeApiKey));
         }
+        for (String id: tweetIds) {
+            twirk.channelMessage(getTweetDetails(id));
+        }
     }
     
-    private ArrayList<String> getUrls(String message, Pattern pattern) {
+    private ArrayList<String> getMatches(String message, Pattern pattern) {
         ArrayList<String> output = new ArrayList<>();
         Matcher matcher = pattern.matcher(message);
         while (matcher.find()) {
@@ -146,6 +165,28 @@ public class LinkListener implements TwirkListener {
                 title,
                 username,
                 numberFormat.format(viewCount)
+        );
+    }
+    
+    private String getTweetDetails(String id) {
+        Status tweet;
+        try {
+            tweet = twitter.tweets().showStatus(Long.parseLong(id));
+        } catch (TwitterException e) {
+            System.out.println("Twitter Exception");
+            return "";
+        }
+        
+        String user = tweet.getUser().getName();
+        String content = tweet.getText().split(" https")[0];
+        int retweets = tweet.getRetweetCount();
+        int likes = tweet.getFavoriteCount();
+        return String.format(
+                "Tweet by @%s: %s • \uD83D\uDD01%d | ❤%d",
+                user,
+                content,
+                retweets,
+                likes
         );
     }
 }
