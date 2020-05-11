4cf9b8ec9421ae6badf5179327b7aa361b41cba0
JCog <jordancogan4@gmail.com>
2020-05-11 18:00:22 -0400

    move settings tags to static Strings

--- src/main/java/Util/Settings.java
+++ src/main/java/Util/Settings.java
@@ -6,6 +6,30 @@ import java.io.File;
 import java.io.IOException;
 
 public class Settings {
+    private static final String INI_FILENAME = "settings.ini";
+    
+    /////////////////////  TAGS  /////////////////////
+    private static final String GENERAL_CAT_TAG = "general";
+    private static final String GENERAL_VERBOSE_TAG = "verbose";
+    
+    private static final String TWITCH_CAT_TAG = "twitch";
+    private static final String TWITCH_STREAM_TAG = "stream";
+    private static final String TWITCH_USERNAME_TAG = "username";
+    private static final String TWITCH_AUTH_TOKEN_TAG = "authToken";
+    
+    private static final String DISCORD_CAT_TAG = "discord";
+    private static final String DISCORD_TOKEN_TAG = "token";
+    
+    private static final String YOUTUBE_CAT_TAG = "youtube";
+    private static final String YOUTUBE_API_KEY_TAG = "apiKey";
+    
+    private static final String TWITTER_CAT_TAG = "twitter";
+    private static final String TWITTER_CONSUMER_KEY_TAG = "consumerKey";
+    private static final String TWITTER_CONSUMER_SECRET_TAG = "consumerSecret";
+    private static final String TWITTER_ACCESS_TOKEN_TAG = "accessToken";
+    private static final String TWITTER_ACCESS_TOKEN_SECRET_TAG = "accessTokenSecret";
+    
+    /////////////////////  VARS  /////////////////////
     private static boolean VERBOSE;
     
     private static String TWITCH_STREAM;
@@ -21,9 +45,9 @@ public class Settings {
     private static String TWITTER_ACCESS_TOKEN;
     private static String TWITTER_ACCESS_TOKEN_SECRET;
     
-    private static final String INI_FILENAME = "settings.ini";
-    
+    //////////////////////////////////////////////////
     public static void init() {
+        //noinspection MismatchedQueryAndUpdateOfCollection
         Wini ini;
         try {
             ini = new Wini(new File(INI_FILENAME));
@@ -34,20 +58,20 @@ public class Settings {
             return;
         }
         
-        VERBOSE = ini.get("general", "verbose", boolean.class);
+        VERBOSE = ini.get(GENERAL_CAT_TAG, GENERAL_VERBOSE_TAG, boolean.class);
+    
+        TWITCH_STREAM = ini.get(TWITCH_CAT_TAG, TWITCH_STREAM_TAG);
+        TWITCH_USERNAME = ini.get(TWITCH_CAT_TAG, TWITCH_USERNAME_TAG);
+        TWITCH_AUTH_TOKEN = ini.get(TWITCH_CAT_TAG, TWITCH_AUTH_TOKEN_TAG);
         
-        TWITCH_STREAM = ini.get("twitch", "stream");
-        TWITCH_USERNAME = ini.get("twitch", "username");
-        TWITCH_AUTH_TOKEN = ini.get("twitch", "authToken");
+        DISCORD_TOKEN = ini.get(DISCORD_CAT_TAG, DISCORD_TOKEN_TAG);
         
-        DISCORD_TOKEN = ini.get("discord", "token");
+        YOUTUBE_API_KEY = ini.get(YOUTUBE_CAT_TAG, YOUTUBE_API_KEY_TAG);
         
-        YOUTUBE_API_KEY = ini.get("youtube", "apiKey");
-        
-        TWITTER_CONSUMER_KEY = ini.get("twitter", "consumerKey");
-        TWITTER_CONSUMER_SECRET = ini.get("twitter", "consumerSecret");
-        TWITTER_ACCESS_TOKEN = ini.get("twitter", "accessToken");
-        TWITTER_ACCESS_TOKEN_SECRET = ini.get("twitter", "accessTokenSecret");
+        TWITTER_CONSUMER_KEY = ini.get(TWITTER_CAT_TAG, TWITTER_CONSUMER_KEY_TAG);
+        TWITTER_CONSUMER_SECRET = ini.get(TWITTER_CAT_TAG, TWITTER_CONSUMER_SECRET_TAG);
+        TWITTER_ACCESS_TOKEN = ini.get(TWITTER_CAT_TAG, TWITTER_ACCESS_TOKEN_TAG);
+        TWITTER_ACCESS_TOKEN_SECRET = ini.get(TWITTER_CAT_TAG, TWITTER_ACCESS_TOKEN_SECRET_TAG);
     }
     
     public static boolean isVerbose() {
