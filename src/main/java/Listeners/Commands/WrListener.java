871abcb81dffce2c2f8f8392a2af25657d539a13
JCog <jordancogan4@gmail.com>
2020-05-17 05:50:22 -0400

    clean up WR logic

--- src/main/java/Listeners/Commands/WrListener.java
+++ src/main/java/Listeners/Commands/WrListener.java
@@ -43,92 +43,67 @@ public class WrListener extends CommandBase {
     protected void performCommand(String command, TwitchUser sender, TwitchMessage message) {
         String title = streamInfo.getTitle().toLowerCase();
         String game = streamInfo.getGame();
-        if (game.equals(GAME_SUNSHINE)) {
-            if (title.contains("any%")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.SUNSHINE, SunshineCategory.ANY_PERCENT)));
-            }
-            else if (title.contains("all episodes")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.SUNSHINE, SunshineCategory.ALL_EPISODES)));
-            }
-            else if (title.contains("79")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.SUNSHINE, SunshineCategory.SHINES_79)));
-            }
-            else if (title.contains("96")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.SUNSHINE, SunshineCategory.SHINES_96)));
-            }
-            else if (title.contains("120")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.SUNSHINE, SunshineCategory.SHINES_120)));
-            }
-            else {
-                twirk.channelMessage(String.format("@%s Unknown WR", sender.getDisplayName()));
-            }
+        String wrText = "Unknown WR";
+        switch (game) {
+            case GAME_SUNSHINE:
+                if (title.contains("any%")) {
+                    wrText = getWr(Game.SUNSHINE, SunshineCategory.ANY_PERCENT);
+                } else if (title.contains("all episodes")) {
+                    wrText = getWr(Game.SUNSHINE, SunshineCategory.ALL_EPISODES);
+                } else if (title.contains("79")) {
+                    wrText = getWr(Game.SUNSHINE, SunshineCategory.SHINES_79);
+                } else if (title.contains("96")) {
+                    wrText = getWr(Game.SUNSHINE, SunshineCategory.SHINES_96);
+                } else if (title.contains("120")) {
+                    wrText = getWr(Game.SUNSHINE, SunshineCategory.SHINES_120);
+                }
+                break;
+            case GAME_PAPER_MARIO:
+                if (title.contains("any% (no peach warp)") || title.contains("any% (no pw)")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.ANY_PERCENT_NO_PW);
+                } else if (title.contains("any% no rng") || title.contains("any% (no rng)")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.ANY_NO_RNG);
+                } else if (title.contains("any%")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.ANY_PERCENT);
+                } else if (title.contains("all cards") && !title.contains("reverse")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.ALL_CARDS);
+                } else if (title.contains("all bosses")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.ALL_BOSSES);
+                } else if (title.contains("glitchless")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.GLITCHLESS);
+                } else if (title.contains("100%")) {
+                    wrText = getWr(Game.PAPER_MARIO, PapeCategory.HUNDO);
+                } else if (title.contains("pig") || title.contains("\uD83D\uDC37") || title.contains("oink")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.PIGGIES);
+                } else if (title.contains("all bloops")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.ALL_BLOOPS);
+                } else if (title.contains("chapter 1")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.BEAT_CHAPTER_1);
+                } else if (title.contains("soapcake") || title.contains("soap cake")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.SOAP_CAKE);
+                } else if (title.contains("reverse") && title.contains("all cards")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.REVERSE_ALL_CARDS);
+                } else if (title.contains("mailman") || title.contains("amazon prime")) {
+                    wrText = getWr(Game.PAPER_MARIO_MEMES, PapeCategory.MAILMAN);
+                }
+                break;
+            case GAME_TTYD:
+                if (title.contains("any%") && (title.contains("japanese") || title.contains("jp"))) {
+                    wrText = getWr(Game.TTYD, TtydCategory.ANY_PERCENT_JP);
+                } else if (title.contains("crystal stars")) {
+                    wrText = getWr(Game.TTYD, TtydCategory.ALL_CRYSTAL_STARS);
+                } else if (title.contains("100%") || title.contains("hundo")) {
+                    wrText = getWr(Game.TTYD, TtydCategory.HUNDO);
+                } else if (title.contains("glitchless")) {
+                    wrText = getWr(Game.TTYD, TtydCategory.GLITCHLESS);
+                } else if (title.contains("collectibles")) {
+                    wrText = getWr(Game.TTYD, TtydCategory.ALL_COLLECTIBLES);
+                } else if (title.contains("upgrades")) {
+                    wrText = getWr(Game.TTYD, TtydCategory.MAX_UPGRADES);
+                }
+                break;
         }
-        else if (game.equals(GAME_PAPER_MARIO)) {
-            if (title.contains("any% (no peach warp)") || title.contains("any% (no pw)")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.ANY_PERCENT_NO_PW)));
-            }
-            else if (title.contains("any% no rng") || title.contains("any% (no rng)")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.ANY_NO_RNG)));
-            }
-            else if (title.contains("any%")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.ANY_PERCENT)));
-            }
-            else if (title.contains("all cards") && !title.contains("reverse")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.ALL_CARDS)));
-            }
-            else if (title.contains("all bosses")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.ALL_BOSSES)));
-            }
-            else if (title.contains("glitchless")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.GLITCHLESS)));
-            }
-            else if (title.contains("100%")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO, PapeCategory.HUNDO)));
-            }
-            else if (title.contains("pig") || title.contains("\uD83D\uDC37") || title.contains("oink")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.PIGGIES)));
-            }
-            else if (title.contains("all bloops")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.ALL_BLOOPS)));
-            }
-            else if (title.contains("chapter 1")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.BEAT_CHAPTER_1)));
-            }
-            else if (title.contains("soapcake") || title.contains("soap cake")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.SOAP_CAKE)));
-            }
-            else if (title.contains("reverse") && title.contains("all cards")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.REVERSE_ALL_CARDS)));
-            }
-            else if (title.contains("mailman") || title.contains("amazon prime")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.PAPER_MARIO_MEMES, PapeCategory.MAILMAN)));
-            }
-            else {
-                twirk.channelMessage(String.format("@%s Unknown WR", sender.getDisplayName()));
-            }
-        }
-        else if (game.equals(GAME_TTYD)) {
-            if (title.contains("any%") && (title.contains("japanese") || title.contains("jp"))) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.ANY_PERCENT_JP)));
-            }
-            else if (title.contains("crystal stars")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.ALL_CRYSTAL_STARS)));
-            }
-            else if (title.contains("100%") || title.contains("hundo")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.HUNDO)));
-            }
-            else if (title.contains("glitchless")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.GLITCHLESS)));
-            }
-            else if (title.contains("collectibles")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.ALL_COLLECTIBLES)));
-            }
-            else if (title.contains("upgrades")) {
-                twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), getWr(Game.TTYD, TtydCategory.MAX_UPGRADES)));
-            }
-            else {
-                twirk.channelMessage(String.format("@%s Unknown WR", sender.getDisplayName()));
-            }
-        }
+        
+        twirk.channelMessage(String.format("@%s %s", sender.getDisplayName(), wrText));
     }
 }
