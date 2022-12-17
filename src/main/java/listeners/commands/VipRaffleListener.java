d8ad9f859c8675ed62dc8c886bf0b3518e9e9658
JCog <jordancogan4@gmail.com>
2022-12-17 01:59:23 -0600

    start implementing raffle selection

--- src/main/java/listeners/commands/VipRaffleListener.java
+++ src/main/java/listeners/commands/VipRaffleListener.java
@@ -1,6 +1,7 @@
 package listeners.commands;
 
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.github.twitch4j.helix.domain.User;
 import database.DbManager;
 import database.entries.VipRaffleItem;
 import database.misc.VipRaffleDb;
@@ -9,6 +10,9 @@ import util.TwitchApi;
 import util.TwitchUserLevel.USER_LEVEL;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ScheduledExecutorService;
 
 public class VipRaffleListener extends CommandBase {
@@ -28,9 +32,20 @@ public class VipRaffleListener extends CommandBase {
     
     @Override
     protected void performCommand(String command, USER_LEVEL userLevel, ChannelMessageEvent messageEvent) {
-    
         if (userLevel == USER_LEVEL.BROADCASTER) {
-            //TODO: perform raffle
+            ArrayList<String> ids = performRaffle(new LinkedList<>(vipRaffleDb.getAllVipRaffleItemsCurrentMonth()));
+            List<User> winners = twitchApi.getUserListByIds(ids);
+            twitchApi.channelMessage(String.format(
+                    "The winners of the raffle are %s, %s, %s, %s, and %s. Congrats on winning VIP for the month! jcogChamp",
+                    winners.get(0).getDisplayName(),
+                    winners.get(1).getDisplayName(),
+                    winners.get(2).getDisplayName(),
+                    winners.get(3).getDisplayName(),
+                    winners.get(4).getDisplayName()
+            ));
+            for (User winner : winners) {
+                System.out.printf("/vip %s%n", winner.getDisplayName());
+            }
         } else {
             String userId = messageEvent.getMessageEvent().getUser().getId();
             String userDisplayName = TwitchEventListener.getDisplayName(messageEvent.getMessageEvent().getTags());
@@ -56,5 +71,30 @@ public class VipRaffleListener extends CommandBase {
         }
         
         
+    }
+    
+    private ArrayList<String> performRaffle(LinkedList<VipRaffleItem> raffleItems) {
+        int totalEntries = 0;
+        for (VipRaffleItem raffleItem : raffleItems) {
+            totalEntries += raffleItem.getEntryCount();
+        }
+        
+        Random random = new Random();
+        ArrayList<String> winnerIds = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int index = 0;
+            int winningIndex = random.nextInt(totalEntries);
+            for (VipRaffleItem raffleItem : raffleItems) {
+                if (winningIndex <= index) {
+                    winnerIds.add(raffleItem.getTwitchId());
+                    raffleItems.remove(raffleItem);
+                    totalEntries -= raffleItem.getEntryCount();
+                    break;
+                }
+                index += raffleItem.getEntryCount();
+            }
+        }
+        
+        return winnerIds;
     }
 }
