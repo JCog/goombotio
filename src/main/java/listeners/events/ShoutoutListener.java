2f6a1d7e26972516e86539278a3c3e8aa0b1e296
JCog <jordancogan4@gmail.com>
2023-12-14 04:40:45 -0600

    only shoutout raiders if the streamer follows them

--- src/main/java/listeners/events/ShoutoutListener.java
+++ src/main/java/listeners/events/ShoutoutListener.java
@@ -1,6 +1,7 @@
 package listeners.events;
 
 import com.github.twitch4j.chat.events.channel.RaidEvent;
+import com.netflix.hystrix.exception.HystrixRuntimeException;
 import listeners.TwitchEventListener;
 import util.TwitchApi;
 
@@ -13,7 +14,16 @@ public class ShoutoutListener implements TwitchEventListener {
     
     @Override
     public void onRaid(RaidEvent raidEvent) {
-        if (raidEvent.getViewers() > 1) {
+        String streamerId = twitchApi.getStreamerUser().getId();
+        String raiderId = raidEvent.getRaider().getId();
+        boolean streamerFollows;
+        try {
+            streamerFollows = twitchApi.getChannelFollower(streamerId, raiderId) != null;
+        } catch (HystrixRuntimeException e) {
+            System.out.printf("Error checking if streamer follows %s after raid.%n", raidEvent.getRaider().getName());
+            return;
+        }
+        if (streamerFollows) {
             twitchApi.shoutout(raidEvent.getRaider().getId());
         }
     }
