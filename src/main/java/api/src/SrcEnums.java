56eafed43a3d6fef17bcde451bd7d01e37be8d48
JCog <42006114+JCog@users.noreply.github.com>
2024-03-26 07:52:35 -0500

    add HM:FOMT to WrListener

--- src/main/java/api/src/SrcEnums.java
+++ src/main/java/api/src/SrcEnums.java
@@ -2,7 +2,7 @@ package api.src;
 
 public class SrcEnums {
     
-    interface Category {
+    public interface Category {
         String getId();
         Game getGame();
         Variable[] getVariables();
@@ -15,12 +15,13 @@ public class SrcEnums {
     
     public enum Game {
         BUG_FABLES("Bug Fables", "w6jlx74d"),
-        SUNSHINE("Super Mario Sunshine", "v1pxjz68"),
+        HM_FOMT("Harvest Moon: Friends of Mineral Town", "9d39lydl"),
+        OOT("The Legend of Zelda: Ocarina of Time", "j1l9qz1g"),
         PAPER_MARIO("Paper Mario", "pdvzq96w"),
         PAPER_MARIO_MEMES("Paper Mario", "pdvz9k96"),
-        TTYD("Paper Mario: The Thousand-Year Door", "m1zjo360"),
-        OOT("The Legend of Zelda: Ocarina of Time", "j1l9qz1g"),
-        SMRPG("Super Mario RPG (Switch)", "kdkqqxld");
+        SMRPG("Super Mario RPG (Switch)", "kdkqqxld"),
+        SMS("Super Mario Sunshine", "v1pxjz68"),
+        TTYD("Paper Mario: The Thousand-Year Door", "m1zjo360");
         
         private final String name;
         private final String id;
@@ -40,39 +41,21 @@ public class SrcEnums {
         }
     }
     
-    public enum PapeVariable implements Variable {
-        N64("r8r5y2le", "jq6vjo71"),
-        WII("r8r5y2le", "5lm2934q"),
-        WII_U("r8r5y2le", "81w7k25q"),
-        SWITCH("r8r5y2le", "jqz2x3kq");
+    public enum HmFomtVariable implements Variable {
+        ANN("onvygrnm", "21dkd34l"),
+        ELLI("onvygrnm", "jqzn4xgq"),
+        HARVEST_GODDESS("onvygrnm", "klr37yjl"),
+        KAREN("onvygrnm", "gq75grv1"),
+        MARY("onvygrnm", "81pyn0g1"),
+        POPURI("onvygrnm", "21gng2nl"),
         
-        private final String varId;
-        private final String valueId;
-        
-        PapeVariable(String varId, String valueId) {
-            this.varId = varId;
-            this.valueId = valueId;
-        }
-        
-        @Override
-        public String getVarId() {
-            return varId;
-        }
-        
-        @Override
-        public String getValueId() {
-            return valueId;
-        }
-    }
-    
-    public enum SmrpgVariable implements Variable {
-        TURBO("p85pvv7l", "q5vo93ml"),
-        NO_TURBO("p85pvv7l", "le2k6x5l");
+        GLITCHED("r8r4k47n", "14o5dv0q"),
+        GLITCHLESS("r8r4k47n", "lx5v4821");
         
         private final String varId;
         private final String valueId;
     
-        SmrpgVariable(String varId, String valueId) {
+        HmFomtVariable(String varId, String valueId) {
             this.varId = varId;
             this.valueId = valueId;
         }
@@ -111,24 +94,71 @@ public class SrcEnums {
         }
     }
     
-    public enum PapeCategory implements Category {
-        PAPE_ANY_PERCENT_NO_ACE("Any% (No ACE)", "z276ozd0", PapeVariable.N64),
-        PAPE_ALL_CARDS("All Cards", "zdn419kq", PapeVariable.N64),
-        PAPE_GLITCHLESS("Glitchless", "jdrx75d6", PapeVariable.N64),
-        PAPE_ANY_PERCENT_NO_PW("Any% (No PW)", "q25q7v2o", PapeVariable.N64),
-        PAPE_ALL_BOSSES("All Bosses", "jdz8vgdv", PapeVariable.N64),
-        PAPE_HUNDO("100%", "02qolyky", PapeVariable.N64),
-        PAPE_REVERSE_ALL_CARDS("Reverse All Cards", "824gw1n2", PapeVariable.N64),
-        PAPE_NMSB("Any% No Major Sequence Breaks", "9d8l6wq2", PapeVariable.N64),
-        PAPE_ANY_PERCENT("Any%", "rklzlr82", PapeVariable.N64);
+    public enum PapeVariable implements Variable {
+        N64("r8r5y2le", "jq6vjo71"),
+        WII("r8r5y2le", "5lm2934q"),
+        WII_U("r8r5y2le", "81w7k25q"),
+        SWITCH("r8r5y2le", "jqz2x3kq");
+        
+        private final String varId;
+        private final String valueId;
+        
+        PapeVariable(String varId, String valueId) {
+            this.varId = varId;
+            this.valueId = valueId;
+        }
+        
+        @Override
+        public String getVarId() {
+            return varId;
+        }
+        
+        @Override
+        public String getValueId() {
+            return valueId;
+        }
+    }
+    
+    public enum SmrpgVariable implements Variable {
+        TURBO("p85pvv7l", "q5vo93ml"),
+        NO_TURBO("p85pvv7l", "le2k6x5l");
+        
+        private final String varId;
+        private final String valueId;
+        
+        SmrpgVariable(String varId, String valueId) {
+            this.varId = varId;
+            this.valueId = valueId;
+        }
+        
+        @Override
+        public String getVarId() {
+            return varId;
+        }
+        
+        @Override
+        public String getValueId() {
+            return valueId;
+        }
+    }
+    
+    public enum BugFablesCategory implements Category {
+        BF_ANY_PERCENT("Any%", "n2yog3zd"),
+        BF_HUNDO("100%", "w20g3v8k"),
+        BF_GLITCHLESS("Any% Glitchless", "9kvo8g32"),
+        BF_ALL_BOSSES("All Bosses", "ndx95yjd"),
+        BF_ALL_CHAPTERS("All Chapters", "wdmxv5ok"),
+        BF_ANY_MYSTERY("Any% MYSTERY?", "824g00m2"),
+        BF_ANY_ALL_CODES("Any% All Codes", "n2yogx1d"),
+        BF_ANY_DLL("Any% dll", "wk6nylx2");
         
         private final String name;
         private final String id;
         private final Variable[] vars;
-    
-        PapeCategory(String name, String id, Variable... vars) {
+        
+        BugFablesCategory(String name, String uri, Variable... vars) {
             this.name = name;
-            this.id = id;
+            this.id = uri;
             this.vars = vars;
         }
         
@@ -139,38 +169,43 @@ public class SrcEnums {
         
         @Override
         public Game getGame() {
-            return Game.PAPER_MARIO;
+            return Game.BUG_FABLES;
         }
-    
+        
         @Override
         public Variable[] getVariables() {
             return vars;
         }
-    
+        
         @Override
         public String toString() {
             return name;
         }
     }
     
-    public enum PapeMemesCategory implements Category {
-        PAPE_PIGGIES("5 Golden Lil' Oinks", "7kjpw44k"),
-        PAPE_ALL_BADGES("All Badges", "02qw5ej2"),
-        PAPE_ALL_BLOOPS("All Bloops", "q257ywwd"),
-        PAPE_ANY_NO_RNG("Any% No RNG", "wk6o43rk"),
-        PAPE_BEAT_CHAPTER_1("Beat Chapter 1", "mkez638k"),
-        PAPE_GLITCHLESS_101("Glitchless 101%", "xd11554d"),
-        PAPE_MAILMAN("Mailman%", "02qo0yjk"),
-        PAPE_SOAP_CAKE("Soap Cake%", "7dgrjl7k"),
-        PAPE_STOP_N_SWOP("Stop 'n' Swop", "z27mv9g2");
+    public enum HmFomtCategory implements Category {
+        HM_ANN_GLITCHED("Marriage (Ann, Glitched)", "xd1mvp4d", HmFomtVariable.ANN, HmFomtVariable.GLITCHED),
+        HM_ANN_GLITCHLESS("Marriage (Ann, Glitchless)", "xd1mvp4d", HmFomtVariable.ANN, HmFomtVariable.GLITCHLESS),
+        HM_ELLI_GLITCHED("Marriage (Elli, Glitched)", "xd1mvp4d", HmFomtVariable.ELLI, HmFomtVariable.GLITCHED),
+        HM_ELLI_GLITCHLESS("Marriage (Elli, Glitchless)", "xd1mvp4d", HmFomtVariable.ELLI, HmFomtVariable.GLITCHLESS),
+        HM_GODDESS_GLITCHED("Marriage (Harvest Goddess, Glitched)", "xd1mvp4d", HmFomtVariable.HARVEST_GODDESS, HmFomtVariable.GLITCHED),
+        HM_GODDESS_GLITCHLESS("Marriage (Harvest Goddess, Glitchless)", "xd1mvp4d", HmFomtVariable.HARVEST_GODDESS, HmFomtVariable.GLITCHLESS),
+        HM_KAREN_GLITCHED("Marriage (Karen, Glitched)", "xd1mvp4d", HmFomtVariable.KAREN, HmFomtVariable.GLITCHED),
+        HM_KAREN_GLITCHLESS("Marriage (Karen, Glitchless)", "xd1mvp4d", HmFomtVariable.KAREN, HmFomtVariable.GLITCHLESS),
+        HM_MARY_GLITCHED("Marriage (Mary, Glitched)", "xd1mvp4d", HmFomtVariable.MARY, HmFomtVariable.GLITCHED),
+        HM_MARY_GLITCHLESS("Marriage (Mary, Glitchless)", "xd1mvp4d", HmFomtVariable.MARY, HmFomtVariable.GLITCHLESS),
+        HM_POPURI_GLITCHED("Marriage (Popuri, Glitched)", "xd1mvp4d", HmFomtVariable.POPURI, HmFomtVariable.GLITCHED),
+        HM_POPURI_GLITCHLESS("Marriage (Popuri, Glitchless)", "xd1mvp4d", HmFomtVariable.POPURI, HmFomtVariable.GLITCHLESS),
+        HM_POWER_BERRIES_GLITCHED("Power Berries (Glitched)", "xd1mvp4d", HmFomtVariable.GLITCHED),
+        HM_POWER_BERRIES_GLITCHLESS("Power Berries (Glitchless)", "xd1mvp4d", HmFomtVariable.GLITCHLESS);
         
         private final String name;
         private final String id;
         private final Variable[] vars;
-        
-        PapeMemesCategory(String name, String id, Variable... vars) {
+    
+        HmFomtCategory(String name, String uri, Variable... vars) {
             this.name = name;
-            this.id = id;
+            this.id = uri;
             this.vars = vars;
         }
         
@@ -181,9 +216,9 @@ public class SrcEnums {
         
         @Override
         public Game getGame() {
-            return Game.PAPER_MARIO_MEMES;
+            return Game.HM_FOMT;
         }
-    
+        
         @Override
         public Variable[] getVariables() {
             return vars;
@@ -231,6 +266,124 @@ public class SrcEnums {
         }
     }
     
+    public enum PapeCategory implements Category {
+        PAPE_ANY_PERCENT_NO_ACE("Any% (No ACE)", "z276ozd0", PapeVariable.N64),
+        PAPE_ALL_CARDS("All Cards", "zdn419kq", PapeVariable.N64),
+        PAPE_GLITCHLESS("Glitchless", "jdrx75d6", PapeVariable.N64),
+        PAPE_ANY_PERCENT_NO_PW("Any% (No PW)", "q25q7v2o", PapeVariable.N64),
+        PAPE_ALL_BOSSES("All Bosses", "jdz8vgdv", PapeVariable.N64),
+        PAPE_HUNDO("100%", "02qolyky", PapeVariable.N64),
+        PAPE_REVERSE_ALL_CARDS("Reverse All Cards", "824gw1n2", PapeVariable.N64),
+        PAPE_NMSB("Any% No Major Sequence Breaks", "9d8l6wq2", PapeVariable.N64),
+        PAPE_ANY_PERCENT("Any%", "rklzlr82", PapeVariable.N64);
+        
+        private final String name;
+        private final String id;
+        private final Variable[] vars;
+        
+        PapeCategory(String name, String id, Variable... vars) {
+            this.name = name;
+            this.id = id;
+            this.vars = vars;
+        }
+        
+        @Override
+        public String getId() {
+            return id;
+        }
+        
+        @Override
+        public Game getGame() {
+            return Game.PAPER_MARIO;
+        }
+        
+        @Override
+        public Variable[] getVariables() {
+            return vars;
+        }
+        
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+    
+    public enum PapeMemesCategory implements Category {
+        PAPE_PIGGIES("5 Golden Lil' Oinks", "7kjpw44k"),
+        PAPE_ALL_BADGES("All Badges", "02qw5ej2"),
+        PAPE_ALL_BLOOPS("All Bloops", "q257ywwd"),
+        PAPE_ANY_NO_RNG("Any% No RNG", "wk6o43rk"),
+        PAPE_BEAT_CHAPTER_1("Beat Chapter 1", "mkez638k"),
+        PAPE_GLITCHLESS_101("Glitchless 101%", "xd11554d"),
+        PAPE_MAILMAN("Mailman%", "02qo0yjk"),
+        PAPE_SOAP_CAKE("Soap Cake%", "7dgrjl7k"),
+        PAPE_STOP_N_SWOP("Stop 'n' Swop", "z27mv9g2");
+        
+        private final String name;
+        private final String id;
+        private final Variable[] vars;
+        
+        PapeMemesCategory(String name, String id, Variable... vars) {
+            this.name = name;
+            this.id = id;
+            this.vars = vars;
+        }
+        
+        @Override
+        public String getId() {
+            return id;
+        }
+        
+        @Override
+        public Game getGame() {
+            return Game.PAPER_MARIO_MEMES;
+        }
+        
+        @Override
+        public Variable[] getVariables() {
+            return vars;
+        }
+        
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+    
+    public enum SmrpgCategory implements Category {
+        SMRPG_NORMAL_RTA_TURBO("Normal RTA", "beat-the-game", SmrpgVariable.TURBO);
+        
+        private final String name;
+        private final String id;
+        private final Variable[] vars;
+        
+        SmrpgCategory(String name, String uri, Variable... vars) {
+            this.name = name;
+            this.id = uri;
+            this.vars = vars;
+        }
+        
+        @Override
+        public String getId() {
+            return id;
+        }
+        
+        @Override
+        public Game getGame() {
+            return Game.SMRPG;
+        }
+        
+        @Override
+        public Variable[] getVariables() {
+            return vars;
+        }
+        
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+    
     public enum SmsCategory implements Category {
         SMS_ANY_PERCENT("Any%", "n2y3r8do"),
         SMS_ALL_EPISODES("All Episodes", "wkpmjjkr"),
@@ -255,48 +408,7 @@ public class SrcEnums {
         
         @Override
         public Game getGame() {
-            return Game.OOT;
-        }
-        
-        @Override
-        public Variable[] getVariables() {
-            return vars;
-        }
-        
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-    
-    public enum BugFablesCategory implements Category {
-        BF_ANY_PERCENT("Any%", "n2yog3zd"),
-        BF_HUNDO("100%", "w20g3v8k"),
-        BF_GLITCHLESS("Any% Glitchless", "9kvo8g32"),
-        BF_ALL_BOSSES("All Bosses", "ndx95yjd"),
-        BF_ALL_CHAPTERS("All Chapters", "wdmxv5ok"),
-        BF_ANY_MYSTERY("Any% MYSTERY?", "824g00m2"),
-        BF_ANY_ALL_CODES("Any% All Codes", "n2yogx1d"),
-        BF_ANY_DLL("Any% dll", "wk6nylx2");
-        
-        private final String name;
-        private final String id;
-        private final Variable[] vars;
-        
-        BugFablesCategory(String name, String uri, Variable... vars) {
-            this.name = name;
-            this.id = uri;
-            this.vars = vars;
-        }
-        
-        @Override
-        public String getId() {
-            return id;
-        }
-        
-        @Override
-        public Game getGame() {
-            return Game.OOT;
+            return Game.SMS;
         }
         
         @Override
@@ -335,41 +447,7 @@ public class SrcEnums {
         
         @Override
         public Game getGame() {
-            return Game.OOT;
-        }
-        
-        @Override
-        public Variable[] getVariables() {
-            return vars;
-        }
-        
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-    
-    public enum SmrpgCategory implements Category {
-        SMRPG_NORMAL_RTA_TURBO("Normal RTA", "beat-the-game", SmrpgVariable.TURBO);
-        
-        private final String name;
-        private final String id;
-        private final Variable[] vars;
-    
-        SmrpgCategory(String name, String uri, Variable... vars) {
-            this.name = name;
-            this.id = uri;
-            this.vars = vars;
-        }
-        
-        @Override
-        public String getId() {
-            return id;
-        }
-        
-        @Override
-        public Game getGame() {
-            return Game.OOT;
+            return Game.TTYD;
         }
         
         @Override
--- src/main/java/listeners/commands/WrListener.java
+++ src/main/java/listeners/commands/WrListener.java
@@ -1,6 +1,7 @@
 package listeners.commands;
 
 import api.src.SrcApi;
+import api.src.SrcEnums;
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import com.github.twitch4j.helix.domain.Stream;
 import com.netflix.hystrix.exception.HystrixRuntimeException;
@@ -9,6 +10,8 @@ import util.TwitchApi;
 import util.TwitchUserLevel.USER_LEVEL;
 
 import static api.src.SrcEnums.BugFablesCategory.*;
+import static api.src.SrcEnums.HmFomtCategory.HM_KAREN_GLITCHED;
+import static api.src.SrcEnums.HmFomtCategory.HM_KAREN_GLITCHLESS;
 import static api.src.SrcEnums.OotCategory.OOT_ANY_PERCENT;
 import static api.src.SrcEnums.OotCategory.OOT_GLITCHLESS_AMQ;
 import static api.src.SrcEnums.PapeCategory.*;
@@ -25,11 +28,12 @@ public class WrListener extends CommandBase {
     private static final String PATTERN = "!wr";
     
     private static final String GAME_ID_BUG_FABLES = "511735";
-    private static final String GAME_ID_SUNSHINE = "6086";
-    private static final String GAME_ID_PAPER_MARIO = "18231";
-    private static final String GAME_ID_TTYD = "6855";
+    private static final String GAME_ID_HM_FOMT = "14599";
     private static final String GAME_ID_OOT = "11557";
+    private static final String GAME_ID_PAPER_MARIO = "18231";
     private static final String GAME_ID_SMRPG_SWITCH = "1675405846";
+    private static final String GAME_ID_SMS = "6086";
+    private static final String GAME_ID_TTYD = "6855";
 
     private final TwitchApi twitchApi;
     private final SrcApi srcApi;
@@ -56,104 +60,122 @@ public class WrListener extends CommandBase {
             streamTitle = stream.getTitle().toLowerCase();
             gameId = stream.getGameId();
         }
-        String wrText = "Unknown WR";
+        
+        SrcEnums.Category category = null;
         switch (gameId) {
             case GAME_ID_BUG_FABLES:
                 if (streamTitle.contains("100%") || streamTitle.contains("hundo") || streamTitle.contains("\uD83D\uDCAF")) {
-                    wrText = srcApi.getWr(BF_HUNDO);
+                    category = BF_HUNDO;
                 } else if (streamTitle.contains("glitchless")) {
-                    wrText = srcApi.getWr(BF_GLITCHLESS);
+                    category = BF_GLITCHLESS;
                 } else if (streamTitle.contains("bosses")) {
-                    wrText = srcApi.getWr(BF_ALL_BOSSES);
+                    category = BF_ALL_BOSSES;
                 } else if (streamTitle.contains("chapters")) {
-                    wrText = srcApi.getWr(BF_ALL_CHAPTERS);
+                    category = BF_ALL_CHAPTERS;
                 } else if (streamTitle.contains("mystery")) {
-                    wrText = srcApi.getWr(BF_ANY_MYSTERY);
+                    category = BF_ANY_MYSTERY;
                 } else if (streamTitle.contains("codes")) {
-                    wrText = srcApi.getWr(BF_ANY_ALL_CODES);
+                    category = BF_ANY_ALL_CODES;
                 } else if (streamTitle.contains("dll")) {
-                    wrText = srcApi.getWr(BF_ANY_DLL);
+                    category = BF_ANY_DLL;
                 } else {
-                    wrText = srcApi.getWr(BF_ANY_PERCENT);
+                    category = BF_ANY_PERCENT;
                 }
                 break;
-            case GAME_ID_SUNSHINE:
+            case GAME_ID_SMS:
                 if (streamTitle.contains("all episodes")) {
-                    wrText = srcApi.getWr(SMS_ALL_EPISODES);
+                    category = SMS_ALL_EPISODES;
                 } else if (streamTitle.contains("79")) {
-                    wrText = srcApi.getWr(SMS_SHINES_79);
+                    category = SMS_SHINES_79;
                 } else if (streamTitle.contains("96")) {
-                    wrText = srcApi.getWr(SMS_SHINES_96);
+                    category = SMS_SHINES_96;
                 } else if (streamTitle.contains("120")) {
-                    wrText = srcApi.getWr(SMS_SHINES_120);
+                    category = SMS_SHINES_120;
                 } else {
-                    wrText = srcApi.getWr(SMS_ANY_PERCENT);
+                    category = SMS_ANY_PERCENT;
                 }
                 break;
             case GAME_ID_PAPER_MARIO:
                 if (streamTitle.contains("any% (no peach warp)") || streamTitle.contains("any% (no pw)")) {
-                    wrText = srcApi.getWr(PAPE_ANY_PERCENT_NO_PW);
+                    category = PAPE_ANY_PERCENT_NO_PW;
                 } else if (streamTitle.contains("any%") &&(streamTitle.contains("no ace") || streamTitle.contains("acephobic"))) {
-                    wrText = srcApi.getWr(PAPE_ANY_PERCENT);
+                    category = PAPE_ANY_PERCENT;
                 } else if (streamTitle.contains("any% no rng") || streamTitle.contains("any% (no rng)")) {
-                    wrText = srcApi.getWr(PAPE_ANY_NO_RNG);
+                    category = PAPE_ANY_NO_RNG;
                 } else if (streamTitle.contains("all cards") && !streamTitle.contains("reverse")) {
-                    wrText = srcApi.getWr(PAPE_ALL_CARDS);
+                    category = PAPE_ALL_CARDS;
                 } else if (streamTitle.contains("all bosses")) {
-                    wrText = srcApi.getWr(PAPE_ALL_BOSSES);
+                    category = PAPE_ALL_BOSSES;
                 } else if (streamTitle.contains("glitchless")) {
-                    wrText = srcApi.getWr(PAPE_GLITCHLESS);
+                    category = PAPE_GLITCHLESS;
                 } else if (streamTitle.contains("100%")) {
-                    wrText = srcApi.getWr(PAPE_HUNDO);
+                    category = PAPE_HUNDO;
                 } else if (streamTitle.contains("reverse") && streamTitle.contains("all cards")) {
-                    wrText = srcApi.getWr(PAPE_REVERSE_ALL_CARDS);
+                    category = PAPE_REVERSE_ALL_CARDS;
                 } else if (streamTitle.contains("pig") || streamTitle.contains("\uD83D\uDC37") || streamTitle.contains("oink")) {
-                    wrText = srcApi.getWr(PAPE_PIGGIES);
+                    category = PAPE_PIGGIES;
                 } else if (streamTitle.contains("all bloops")) {
-                    wrText = srcApi.getWr(PAPE_ALL_BLOOPS);
+                    category = PAPE_ALL_BLOOPS;
                 } else if (streamTitle.contains("chapter 1")) {
-                    wrText = srcApi.getWr(PAPE_BEAT_CHAPTER_1);
+                    category = PAPE_BEAT_CHAPTER_1;
                 } else if (streamTitle.contains("soapcake") || streamTitle.contains("soap cake")) {
-                    wrText = srcApi.getWr(PAPE_SOAP_CAKE);
+                    category = PAPE_SOAP_CAKE;
                 } else if (streamTitle.contains("mailman") || streamTitle.contains("amazon prime")) {
-                    wrText = srcApi.getWr(PAPE_MAILMAN);
+                    category = PAPE_MAILMAN;
                 } else if (streamTitle.contains("no major sequence breaks") || streamTitle.contains("nmsb")) {
-                    wrText = srcApi.getWr(PAPE_NMSB);
+                    category = PAPE_NMSB;
                 } else if (streamTitle.contains("swop")) {
-                    wrText = srcApi.getWr(PAPE_STOP_N_SWOP);
+                    category = PAPE_STOP_N_SWOP;
                 } else {
-                    wrText = srcApi.getWr(PAPE_ANY_PERCENT);
+                    category = PAPE_ANY_PERCENT;
                 }
                 break;
             case GAME_ID_TTYD:
                 if (streamTitle.contains("crystal stars")) {
-                    wrText = srcApi.getWr(TTYD_ALL_CRYSTAL_STARS);
+                    category = TTYD_ALL_CRYSTAL_STARS;
                 } else if (streamTitle.contains("100%") || streamTitle.contains("hundo")) {
-                    wrText = srcApi.getWr(TTYD_HUNDO);
+                    category = TTYD_HUNDO;
                 } else if (streamTitle.contains("glitchless")) {
-                    wrText = srcApi.getWr(TTYD_GLITCHLESS);
+                    category = TTYD_GLITCHLESS;
                 } else if (streamTitle.contains("collectibles")) {
-                    wrText = srcApi.getWr(TTYD_ALL_COLLECTIBLES);
+                    category = TTYD_ALL_COLLECTIBLES;
                 } else if (streamTitle.contains("upgrades")) {
-                    wrText = srcApi.getWr(TTYD_MAX_UPGRADES);
+                    category = TTYD_MAX_UPGRADES;
                 } else {
-                    wrText = srcApi.getWr(TTYD_ANY_PERCENT);
+                    category = TTYD_ANY_PERCENT;
                 }
                 break;
             case GAME_ID_OOT:
                 if (streamTitle.contains("swop")) {
-                    wrText = srcApi.getWr(PAPE_STOP_N_SWOP);
+                    category = PAPE_STOP_N_SWOP;
                 } else if (streamTitle.contains("glitchless")){
-                    wrText = srcApi.getWr(OOT_GLITCHLESS_AMQ);
+                    category = OOT_GLITCHLESS_AMQ;
                 } else {
-                    wrText = srcApi.getWr(OOT_ANY_PERCENT);
+                    category = OOT_ANY_PERCENT;
                 }
                 break;
             case GAME_ID_SMRPG_SWITCH:
-                wrText = srcApi.getWr(SMRPG_NORMAL_RTA_TURBO);
+                category = SMRPG_NORMAL_RTA_TURBO;
+                break;
+            case GAME_ID_HM_FOMT:
+                if (streamTitle.contains("karen")) {
+                    if (streamTitle.contains("glitchless")) {
+                        category = HM_KAREN_GLITCHLESS;
+                    } else {
+                        category = HM_KAREN_GLITCHED;
+                    }
+                }
                 break;
         }
     
-        twitchApi.channelMessage(String.format("@%s %s", messageEvent.getUser().getName(), wrText));
+        if (category == null) {
+            twitchApi.channelMessage("Unknown WR");
+            return;
+        }
+        twitchApi.channelMessage(String.format(
+                "@%s %s",
+                messageEvent.getUser().getName(),
+                srcApi.getWr(category)
+        ));
     }
 }
