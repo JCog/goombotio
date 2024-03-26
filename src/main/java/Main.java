01d68cb8bdb78dd0e3c29bddb7adb82374da0eb7
JCog <42006114+JCog@users.noreply.github.com>
2024-03-26 11:01:30 -0500

    print time of on startup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import static java.lang.System.exit;
 import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z").withZone(ZoneId.systemDefault());
+        out.println(dtf.format(Instant.now()));
         out.println("Starting...");
         MainBotController mainBotController = new MainBotController();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            out.println("Stopping...\n");
+            out.print("Stopping... ");
             mainBotController.closeAll();
+            out.println("done.\n");
         }));
 
         //primary loop
