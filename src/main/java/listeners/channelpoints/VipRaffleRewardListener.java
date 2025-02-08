b2e4d404cd74afa372708eb1e51e288d23c3bfdd
JCog <42006114+JCog@users.noreply.github.com>
2025-02-08 07:46:54 -0600

    add output cooldown for raffle entry redemptions

--- src/main/java/listeners/channelpoints/VipRaffleRewardListener.java
+++ src/main/java/listeners/channelpoints/VipRaffleRewardListener.java
@@ -12,7 +12,11 @@ import listeners.TwitchEventListener;
 import util.CommonUtils;
 import util.TwitchApi;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,10 +26,15 @@ import static java.lang.System.out;
 public class VipRaffleRewardListener implements TwitchEventListener {
     private static final String RAFFLE_REWARD_TITLE = "VIP Raffle Entry";
     private static final int ENTRY_COUNT = 1;
+    private static final int COOLDOWN_PERIOD = 60; // seconds
+    private static final int COOLDOWN_TRIGGER = 4;
     
     private final TwitchApi twitchApi;
     private final VipDb vipDb;
     private final VipRaffleDb vipRaffleDb;
+    private final Deque<Instant> timestamps = new LinkedList<>();
+    
+    private boolean cooldownActive = false;
     
     public VipRaffleRewardListener(CommonUtils commonUtils) {
         twitchApi = commonUtils.twitchApi();
@@ -65,13 +74,15 @@ public class VipRaffleRewardListener implements TwitchEventListener {
             int entryCount;
             if (vipRaffleItem != null) {
                 entryCount = vipRaffleItem.entryCount();
-                twitchApi.channelMessage(String.format("@%s You now have %d entr%s! (~%.1f%% of all entries)",
-                        displayName,
-                        entryCount,
-                        entryCount == 1 ? "y" : "ies",
-                        (float) entryCount * 100 / vipRaffleDb.getTotalEntryCountCurrentMonth()
-                ));
                 shouldFulfill = true;
+                if (!checkForCooldown()) {
+                    twitchApi.channelMessage(String.format("@%s You now have %d entr%s! (~%.1f%% of all entries)",
+                            displayName,
+                            entryCount,
+                            entryCount == 1 ? "y" : "ies",
+                            (float) entryCount * 100 / vipRaffleDb.getTotalEntryCountCurrentMonth()
+                    ));
+                }
             } else {
                 out.printf("Error getting entry total for %s%n", displayName);
                 shouldFulfill = false;
@@ -93,6 +104,27 @@ public class VipRaffleRewardListener implements TwitchEventListener {
                             shouldFulfill ? "fulfilling" : "refunding"
                     )
             );
+        }
+    }
+    
+    private boolean checkForCooldown() {
+        timestamps.addLast(Instant.now());
+        if (timestamps.size() < COOLDOWN_TRIGGER) {
+            return false;
+        }
+        
+        if (ChronoUnit.SECONDS.between(timestamps.pollFirst(), timestamps.getLast()) < COOLDOWN_PERIOD) {
+            if (!cooldownActive) {
+                twitchApi.channelAnnouncement(
+                        "Temporarily suppressing raffle output to prevent spam. Don't worry, your raffle entries are " +
+                        "still being recorded!"
+                );
+            }
+            cooldownActive = true;
+            return true;
+        } else {
+            cooldownActive = false;
+            return false;
         }
     }
 }
