6b825984c7fff7ce5e0a10aa05dead969fa9196a
JCog <42006114+JCog@users.noreply.github.com>
2025-09-02 12:49:30 -0500

    fix $(toUser)

--- src/main/java/util/MessageExpressionParser.java
+++ src/main/java/util/MessageExpressionParser.java
@@ -244,7 +244,7 @@ public class MessageExpressionParser {
                 if (commandItem == null) {
                     return ERROR_NON_COMMAND;
                 }
-                if (userArgs.length != 0) {
+                if (userArgs.length != 0 && !userArgs[0].isEmpty()) {
                     String username = userArgs[0].startsWith("@") ? userArgs[0].substring(1) : userArgs[0];
                     User user;
                     try {
